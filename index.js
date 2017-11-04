efafa32f39b48459be7470d8bdaf8f5b88fcfee2
Balthazar Gronon <bgronon@gmail.com>
2017-11-04 14:19:13 -0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const request = require('request')
 
 const server = express()
 
-server.get('/embed', (req, res) => {
+server.get('/oembed', (req, res) => {
+
+  const url = 'https://sigsev.github.io/embed/fcc/evolution/'
+  console.log(req.params.url)
+  // request(`https://raw.githubusercontent.com/SIGSEV/embed/gh-pages/fcc/evolution/embed.json`)
   res.send({})
 })
 
