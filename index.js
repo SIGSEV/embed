9a6c15f06d5322f89a0341163cf1d1c77770a870
Balthazar Gronon <bgronon@gmail.com>
2017-11-04 15:59:34 -0700

    o

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ const request = require('request')
 const server = express()
 
 server.get('/', (req, res) => {
-  const url = 'https://sigsev.github.io/embed/fcc/evolution/'
-  console.log(req.query.url)
-  // request(`https://raw.githubusercontent.com/SIGSEV/embed/gh-pages/fcc/evolution/embed.json`)
-  res.send({})
+  if (!req.query.url) {
+    return res.status(400).end()
+  }
+
+  const path = req.query.url
+    .replace(/\/$/, '')
+    .split('/')
+    .slice(-2)
+    .join('/')
+
+  request(
+    `https://raw.githubusercontent.com/SIGSEV/embed/gh-pages/${path}/embed.json`,
+    (err, res) => {
+      if (err || res.statusCode > 399) {
+        return res.status(400).end()
+      }
+
+      res.send(JSON.parse(res.body))
+    },
+  )
 })
 
 const port = 4500
